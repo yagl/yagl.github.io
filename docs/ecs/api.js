d0c701f5e82873fab8a88dbd45bb8b04a2228fb9
Pierre Beaujeu <pierre.beaujeu@rednet.io>
2015-09-22 17:56:44 +0200

    added documentation for ECS project

---
+++ docs/ecs/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ECS"
+    ],
+    "modules": [
+        "ecs"
+    ],
+    "allModules": [
+        {
+            "displayName": "ecs",
+            "name": "ecs",
+            "description": "Entity Component System module"
+        }
+    ],
+    "elements": []
+} };
+});
---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+
+var fs = require('fs');
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+
+try {
+  fs.statSync('docs');
+} catch(e) {
+  fs.mkdir('docs');
+}
+
+gulp.task('ecs-doc', shell.task([
+  'yuidoc -o docs/ecs yammo-ecs'
+]));
+
+//console.log('ooooooooooooooooooka')
