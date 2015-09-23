32633bd55072dc59dc6dd32800e4dfb66da0b9bc
Pierre Beaujeu <pierre.beaujeu@rednet.io>
2015-09-24 00:31:39 +0200

    add tiledmap and behavior3 docs

---
+++ docs/tiledmap/api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "TiledMap"
+    ],
+    "modules": [
+        "tiledmap"
+    ],
+    "allModules": [
+        {
+            "displayName": "tiledmap",
+            "name": "tiledmap"
+        }
+    ],
+    "elements": []
+} };
+});
--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,8 @@ try {
 
 gulp.task('docs', shell.task([
   'yuidoc -o docs/ecs yagl-ecs/src -t node_modules/yuidoc-lucid-theme -H node_modules/yuidoc-lucid-theme/helpers/helpers.js',
-  'yuidoc -o docs/behavior3 yagl-behavior3/src -t node_modules/yuidoc-lucid-theme -H node_modules/yuidoc-lucid-theme/helpers/helpers.js'
+  'yuidoc -o docs/behavior3 yagl-behavior3/lib -t node_modules/yuidoc-lucid-theme -H node_modules/yuidoc-lucid-theme/helpers/helpers.js',
+  'yuidoc -o docs/tiledmap yagl-tiledmap/src -t node_modules/yuidoc-lucid-theme -H node_modules/yuidoc-lucid-theme/helpers/helpers.js'
 ]));
 
 gulp.task('default', ['docs']);
